4630003dfc3cdfc4715d29c25585888ac772e5c4
charles <nccharles1@gmail.com>
2019-03-07 17:06:20 +0200

    changed structure

--- trwanda/js/main.js
+++ trwanda/js/main.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', function() {
 console.log(window.scrollY)
-if(window.scrollY>=500){
+if(window.scrollY>=100){
     // document.querySelector("scrolled").style.display='block'
     document.querySelector('#scrolled').classList.add('scrolled')
 }else{
